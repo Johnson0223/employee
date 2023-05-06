8318e1cd3c8f041d6f279bf3e73e933d060f482d
Johnson0223 <100580297+Johnson0223@users.noreply.github.com>
2023-05-06 19:36:56 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-customhost = "employee.cauvi0l59bvz.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "Bait3273"
-customdb = "employee"
-custombucket = "ngjunchen-employee"
-customregion = "us-east-1"
-
