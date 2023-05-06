c088440df6aab71b67baafc79245fa70bbe19bbf
Johnson0223 <100580297+Johnson0223@users.noreply.github.com>
2023-05-06 19:21:40 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -23,7 +23,7 @@ table = 'employee'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    return render_template('AddEmp.html')
 
 @app.route("/employees", methods=['GET'])
 def employees():
--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "employee.cauvi0l59bvz.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "ngjunchen-bucket"
+custombucket = "ngjunchen-employee"
 customregion = "us-east-1"
 
